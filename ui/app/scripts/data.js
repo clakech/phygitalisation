8bd0bb92507ca74dae83fbf2e01345aa8ee7a88a
clakech <cyril.lakech@adeoservices.com>
2015-03-12 16:30:56 +0100

    small stuff

--- ui/app/scripts/data.js
+++ ui/app/scripts/data.js
@@ -27,6 +27,69 @@ angular.module('data',[]).constant('Data', {
                 load: ['leger'],
                 image: '25-chevilles-a-clouer-outifix-6x40mm-pour-tous-types-de-materiaux.jpg'
             },
+            {
+                id: 1,
+                name: 'Chevilles + vis à visser RED HEAD, 12 x 30mm',
+                price: 0.75,
+                type: 'visser',
+                wall: ['platre'],
+                load: ['leger'],
+                image: '4-chevilles-vis-a-visser-red-head-12-x-30mm.jpg'
+            },
+            {
+                id: 2,
+                name: 'Chevilles + vis à frapper STANDERS, 5 x 30mm',
+                price: 0.15,
+                type: 'frapper',
+                wall: ['plein'],
+                load: ['lourd'],
+                image: '4-chevilles-vis-a-visser-red-head-12-x-30mm.jpg'
+            },
+            {
+                id: 3,
+                name: 'Chevilles à clouer OUTIFIX 6x40mm pour tous types de matériaux',
+                price: 0.20,
+                type: 'clouer',
+                wall: ['platre','plein'],
+                load: ['leger'],
+                image: '25-chevilles-a-clouer-outifix-6x40mm-pour-tous-types-de-materiaux.jpg'
+            },
+            {
+                id: 1,
+                name: 'Chevilles + vis à visser RED HEAD, 12 x 30mm',
+                price: 0.75,
+                type: 'visser',
+                wall: ['platre'],
+                load: ['leger'],
+                image: '4-chevilles-vis-a-visser-red-head-12-x-30mm.jpg'
+            },
+            {
+                id: 2,
+                name: 'Chevilles + vis à frapper STANDERS, 5 x 30mm',
+                price: 0.15,
+                type: 'frapper',
+                wall: ['plein'],
+                load: ['lourd'],
+                image: '4-chevilles-vis-a-visser-red-head-12-x-30mm.jpg'
+            },
+            {
+                id: 3,
+                name: 'Chevilles à clouer OUTIFIX 6x40mm pour tous types de matériaux',
+                price: 0.20,
+                type: 'clouer',
+                wall: ['platre','plein'],
+                load: ['leger'],
+                image: '25-chevilles-a-clouer-outifix-6x40mm-pour-tous-types-de-materiaux.jpg'
+            },
+            {
+                id: 3,
+                name: 'Chevilles à clouer OUTIFIX 6x40mm pour tous types de matériaux',
+                price: 0.20,
+                type: 'clouer',
+                wall: ['platre','plein'],
+                load: ['leger'],
+                image: '25-chevilles-a-clouer-outifix-6x40mm-pour-tous-types-de-materiaux.jpg'
+            },
         ],
         criteriaById: {
             'wall': {
@@ -45,7 +108,7 @@ angular.module('data',[]).constant('Data', {
             },
             'load': {
                 id: 'load',
-                description: 'poids',
+                description: 'Poids',
                 segment: [
                     {
                         description: 'lourd',
