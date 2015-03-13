585aded6d9dcbb7c12d736e645e16f60366bf371
clakech <cyril.lakech@adeoservices.com>
2015-03-13 11:43:40 +0100

    add a sample for neopixel

--- neopixel/neopixel.ino
+++ neopixel/neopixel.ino
@@ -1,14 +1,14 @@
 #include <Adafruit_NeoPixel.h>
 
-#define PRODUCT_NB_LED 11
-#define PRODUCT_PIN 7
+#define NB_LED 11
+#define PIN 7
 
 Adafruit_NeoPixel *bus;
 
 void setup() {
   Serial.begin(115200);
 
-  bus = new Adafruit_NeoPixel(PRODUCT_NB_LED, PRODUCT_PIN);
+  bus = new Adafruit_NeoPixel(NB_LED, PIN);
   bus->begin();
   bus->show();
 
@@ -40,7 +40,7 @@ void loop() {
 
 void changeColor(String command, uint32_t color) {
   if (command[1] == 'A') {
-    for (int i = 0 ; i < PRODUCT_NB_LED; i++){
+    for (int i = 0 ; i < NB_LED; i++){
       bus->setPixelColor(i, color);
     }
   } 
---
+++ neosimple/neosimple.ino
@@ -0,0 +1,37 @@
+#include <Adafruit_NeoPixel.h>
+
+#define NB_LED 11
+#define PIN 7
+
+Adafruit_NeoPixel *bus;
+
+void setup() {
+  Serial.begin(115200);
+
+  bus = new Adafruit_NeoPixel(NB_LED, PIN);
+  bus->begin();
+  bus->show();
+}
+
+void loop() {
+  for(int i = 0; i < NB_LED ; i++) {
+
+    bus->setPixelColor(i, bus->Color(0,150,0)); 
+
+    bus->show();
+    
+    delay(200); 
+
+  }
+  
+  for(int i = 0; i < NB_LED ; i++) {
+
+    bus->setPixelColor(i, bus->Color(0,0,150)); 
+
+    bus->show();
+    
+    delay(200); 
+
+  }
+}
+
