c65238b53005d1a997d7bc5a80b8d340aadc9833
clakech <cyril.lakech@adeoservices.com>
2015-03-18 12:10:31 +0100

    simplication 3

--- api/app.js
+++ api/app.js
@@ -1,15 +1,12 @@
-var config = require('./config');
 var arduino = require('./arduino');
 var ledstrip = require('./ledstrip');
 var expressApp = require('./express');
 
 var ledStripPromise;
 
-if (config.mock) {
-    ledStripPromise = arduino.connectMock();
-} else {
-    ledStripPromise = arduino.connect(config.arduinoAddress);
-}
+var arduinoAddress = '/dev/tty.usbserial-A96TTZ3N'; // /dev/ttyAMA0 CLH:/dev/tty.usbserial-A96TTZ3N BGR:/dev/ttyUSB0
+
+ledStripPromise = arduino.connect(arduinoAddress); // arduino.connectMock();
 
 ledStripPromise.then(function (arduino) {
 
