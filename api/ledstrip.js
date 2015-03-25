e7196be2f1adfa5de002721b63880d1a0ae762df
Bertrand Gressier <bertrand.gressier@adeoservices.com>
2015-03-25 11:38:27 +0100

    api: n'appelle plus la commande show à chaque appel d'allumage de led

--- api/ledstrip.js
+++ api/ledstrip.js
@@ -1,11 +1,17 @@
+var _ = require('underscore');
+
 var Ledstrip = function (arduinoCommand) {
     this.arduino = arduinoCommand;
 };
 
+var show = _.debounce(function(arduino){
+    arduino.send('S');
+}, 20);
+
 Ledstrip.prototype.on = function (led) {
-    var self = this;
+    var self= this;
     return this.arduino.send('1' + led).then(function () {
-        return self.arduino.send('S');
+        show(self.arduino);
     });
 };
 
